0355d54e4f4da62b2d90aec4fceab912a228525b
Mark <mark.nrecaj0@gmail.com>
2022-12-09 13:50:53 +0100

    challange2 added

--- ushtrime/day_1.php
+++ ushtrime/day_1.php
@@ -4,7 +4,7 @@
 function doublechar2($string, $times)
 {
     $spString = str_split($string);
-    
+
 
     $filtred = [];
     for ($i = 0; $i < count($spString); $i++) {
@@ -20,4 +20,20 @@ function doublechar2($string, $times)
 }
 
 
-print_r(doublechar2("flossskk", 2));
+// print_r(doublechar2("flossskk", 2));
+
+function challange2($arr, $y, $z)
+{
+    $newarr = [];
+    for ($i = 0; $i < count($arr); $i++) {
+        if ($arr[$i] > $z && count($newarr) < $y) {
+            $newarr[] =$arr[$i];
+        }
+    }
+    return $newarr;
+}
+
+print_r(challange2([12, 1, 405, 390, 1093,401,402,403,40, 1000], 4, 400));
+
+// $newarr = [1,2];
+// print_r($newarr);
